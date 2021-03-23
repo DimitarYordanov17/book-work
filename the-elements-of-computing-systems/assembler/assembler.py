c4566240c9de4e892d1fbbe076811b1f2e8b0452
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-03-23 23:06:42 +0200

    add assembler.py blueprint

---
+++ the-elements-of-computing-systems/assembler/assembler.py
@@ -0,0 +1,19 @@
+# A Python assembler for the Hack machine language. @DimitarYordanov17
+# The whole idea is to make one main Assembler class, which implement the following functionalities, in order to sucessfully translate .asm (Hack assembly) to .hack (Hack machine code) programs
+# 1. Symbolic preprocessor
+#   1.1. Jump preprocessor
+#   1.2. Variables preprocessor
+# 2. Parser
+# 3. Code translator
+
+class Assembler:
+  '''
+  Main class, with no need of initialization, which offers an assemble functionality, to convert .asm to .hack files
+  '''
+
+  def assemble():
+    '''
+    Make use of class methods to reach the goal
+    '''
+    return 0
+
