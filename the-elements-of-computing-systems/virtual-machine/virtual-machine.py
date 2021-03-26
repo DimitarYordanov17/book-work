036a586695fff1a3464d86a34e23a2840ddaa54a
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-03-26 19:28:15 +0200

    add initial virtual-machine file and tests

---
+++ the-elements-of-computing-systems/virtual-machine/virtual-machine.py
@@ -0,0 +1,6 @@
+# A virtual machine translator. Intermediate code (supplied by a to be implemented compiler) -> Hack machine language. @DimitarYordanov17
+
+class VirtualMachineTranslator:
+  """
+  To be implemented
+  """
