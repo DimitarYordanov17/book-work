110428e437e5d29c7be30f3c30082cc745dc103d
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-03-27 22:50:44 +0200

    add library blueprint

--- the-elements-of-computing-systems/virtual-machine/virtualMachineLibrary.py
+++ the-elements-of-computing-systems/virtual-machine/virtualMachineLibrary.py
@@ -1,7 +1,50 @@
 # A bytecode library for the VM code > Hack machine language translation. @DimitarYordanov17
 
+
 class VirtualMachineLibrary:
   '''
-  To be implemented
+  Main class to map the Virtual Machine intermediate language to Hack machine language
   '''
-  return 0
+
+  def _primary(operation):
+    '''
+    Define primitive operations, which are going to be main building 'blocks' of higher (arithmetic/memory) instruction
+    '''
+    # TODO: Add full primitive operations
+    bytecode_dictionary = {
+    "sp++": [...],
+    "sp--": [...],
+    ...
+    }
+
+    return bytecode_dictionary[operation]
+
+  def get_arithmetic(instruction):
+    '''
+    Returns bytecode for arithmetic instructions
+    '''
+    # TODO: Add full commands
+
+  def get_memory(instruction, segment, index):
+    '''
+    Makes a call to calculate address and returns full memory bytecode
+    '''
+    # TODO: Add full commands
+
+  def _get_address_calculation(segment, index):
+    '''
+    Returns bytecode segment + index calculation
+    '''
+    # TODO: Add address calculation
+    
+  def get_function(instruction):
+    '''
+    Not implemented in v1
+    '''
+    return 0
+
+  def get_flow(instruction):
+    '''
+    Not implemented in v1
+    '''
+    return 0
