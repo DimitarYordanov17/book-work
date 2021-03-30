6ccad3071961c0ddad08e88f806d19ff80b7730c
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-03-30 17:23:56 +0300

    add directory parsing + fix inconsistency

--- the-elements-of-computing-systems/virtual-machine/second-stage/virtualMachine.py
+++ the-elements-of-computing-systems/virtual-machine/second-stage/virtualMachine.py
@@ -6,74 +6,92 @@ from virtualMachineLibrary import VirtualMachineLibrary
 import os
 import sys
 
+
 class VirtualMachineTranslator:
-  """
-  Main class, capable of processing a single .vm (intermediate code) file, which consist of only stack + memory instructions
-  """
-
-  def translate(input_file_name): 
     """
-    Handle the step-by-step proccess of translating a file
+    Main class, capable of processing a full directory, with .vm files, resulting in one .asm file
     """
 
-    output_file_name = input_file_name.split(".")[0] + ".asm"
-    os.system(f"cp {input_file_name} {output_file_name}")
-    VirtualMachineTranslator.clean(output_file_name)
-    VirtualMachineTranslator.parse_file(output_file_name)
+    def translate_files(path):
+        """
+        Translate all .vm files in path
+        """
 
-  def parse_file(output_file_name):
-    """
-    Parse every instruction and write the requested and further translated equivalent
-    """
+        if '.' in path:  # We have a file
+            output_file_name = path.split(".")[0] + ".asm"
+            os.system(f"cp {path} {output_file_name}")
+            VirtualMachineTranslator.clean(output_file_name)
+            VirtualMachineTranslator.parse_file(output_file_name)
 
-    with open(output_file_name, 'r+') as output_file:
-      total_instructions = 0
-      instructions = output_file.readlines()
-      output_file.seek(0)
+        else:  # We have a directory
+            vm_files = []
 
-      for line in instructions:
-        instruction_structure = line.split()
-        instruction = instruction_structure[0]
-        
-        bytecode_instruction = []
+            for root, dirs, files in os.walk(path):
 
-        if len(instruction_structure) == 1 and instruction != "return": # Stack arithmetic
-          bytecode_instruction = VirtualMachineLibrary.get_arithmetic(instruction, total_instructions)
+                for file_name in files:
+                    file_extension = file_name.split('.')[1]
 
-        elif instruction in ['pop', 'push']: # Memory access
-          bytecode_instruction = VirtualMachineLibrary.get_memory(line, output_file_name.split('.')[0])
-          
-        elif len(instruction_structure) == 2: # Program flow
-          # not implemented in first-stage
-          pass
-        else: # Function calling
-          # not implemented in first-stage
-          pass
-        
-        output_file.write(f"// {line}")
+                    if file_extension == 'vm':
+                        vm_files.append(file_name)
 
-        for instruction in bytecode_instruction:
-          total_instructions += 1
-          output_file.write(instruction + '\n')
+            # Try recursion?
+            for vm_file in vm_files:
+                VirtualMachineTranslator.translate(vm_file)
 
-      output_file.truncate()
-    
-  def clean(input_file):
-    '''
-    Remove unnecesary whitespaces and comments
-    '''
+    def parse_file(input_file_name):
+        """
+        Parse every instruction and write the requested and further translated equivalent
+        """
 
-    with open(input_file, "r+") as f:
-      lines = f.readlines()
-      f.seek(0)
-      for line in lines:
-        if line != '\n':
-          if "//" in line:
-            line_elements = line.lstrip().split("//")
-            if line_elements[0]:
-              f.write(line_elements[0].rstrip() + '\n')
-          else:
-            f.write(line)
-      f.truncate()
+        with open(input_file_name, 'r+') as input_file:
+            total_instructions = 0
+            instructions = input_file.readlines()
+            input_file.seek(0)
 
-VirtualMachineTranslator.translate(sys.argv[1])
+            for line in instructions:
+                instruction_structure = line.split()
+                instruction = instruction_structure[0]
+
+                bytecode_instruction = []
+
+                if len(instruction_structure) == 1 and instruction != "return":  # Stack arithmetic
+                    bytecode_instruction = VirtualMachineLibrary.get_arithmetic(instruction, total_instructions)
+
+                elif instruction in ['pop', 'push']:  # Memory access
+                    bytecode_instruction = VirtualMachineLibrary.get_memory(line, input_file_name.split('.')[0])
+
+                elif len(instruction_structure) == 2:  # Program flow
+                    # not implemented in first-stage
+                    pass
+                else:  # Function calling
+                    # not implemented in first-stage
+                    pass
+
+                input_file.write(f"// {line}")
+
+                for instruction in bytecode_instruction:
+                    total_instructions += 1
+                    input_file.write(instruction + '\n')
+
+            input_file.truncate()
+
+    def clean(input_file):
+        """
+        Remove unnecesary whitespaces and comments
+        """
+
+        with open(input_file, "r+") as f:
+            lines = f.readlines()
+            f.seek(0)
+            for line in lines:
+                if line != '\n':
+                    if "//" in line:
+                        line_elements = line.lstrip().split("//")
+                        if line_elements[0]:
+                            f.write(line_elements[0].rstrip() + '\n')
+                    else:
+                        f.write(line)
+            f.truncate()
+
+
+VirtualMachineTranslator.translate_files(sys.argv[1])
