4cc3609637a2bb86ffe4cb9bd16cd69920046778
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-04-04 14:21:53 +0300

    fix typo

--- the-elements-of-computing-systems/virtual-machine/second-stage/virtualMachineLibrary.py
+++ the-elements-of-computing-systems/virtual-machine/second-stage/virtualMachineLibrary.py
@@ -1,4 +1,4 @@
-# A bytecode library for the VM code > Hack machine language translation. @DimitarYordanov17
+# An assembly library for the VM code > Hack machine language translation. @DimitarYordanov17
 
 class VirtualMachineLibrary:
     """
