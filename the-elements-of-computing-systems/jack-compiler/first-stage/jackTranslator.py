7d144547818b42d19b8ac725ea4bd4cbd84c017f
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-04-13 17:30:03 +0300

    refactor: refactor main translator file

--- the-elements-of-computing-systems/jack-compiler/first-stage/jackTranslator.py
+++ the-elements-of-computing-systems/jack-compiler/first-stage/jackTranslator.py
@@ -10,7 +10,7 @@ import sys
 
 class JackTranslator:
     """
-    Main class, capable of processing a full directory, with .jack files, resutling in corresponding .vm files + ?OS (standard library) files
+    Main class, capable of processing a full directory, with .jack files, resutling in corresponding .vm files files
     """
 
     def translate(path, add_standard_library):
@@ -18,17 +18,23 @@ class JackTranslator:
         Translate a directory, resulting in len(path .jack files) .vm files
         """
 
+        # available in second-stage
+
         return 0
 
     def construct_xml(input_file_name):
         """
-        Parses a single .jack file, resulting in .xml file
+        Parses a single .jack file, resulting in a .xml file
         """
+
         output_file_name = input_file_name.split(".")[0] + ".xml"
         os.system(f"cp {input_file_name} {output_file_name}")
+
         JackTranslatorLibrary.clean(output_file_name)
         JackTranslatorLibrary.tokenize(output_file_name)
+
         JackTranslatorLibrary.parse_file(output_file_name)
+
         JackTranslatorLibrary.tabularize(output_file_name)
 
 JackTranslator.construct_xml(sys.argv[1])
