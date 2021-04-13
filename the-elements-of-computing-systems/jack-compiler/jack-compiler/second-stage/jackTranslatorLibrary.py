c528ae6bd19de5cb7580ff2ec395e6ab441668ec
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-04-13 16:08:52 +0300

    fix file structure

--- the-elements-of-computing-systems/jack-compiler/jack-compiler/second-stage/jackTranslatorLibrary.py
+++
@@ -1,8 +0,0 @@
-# An intermediate code library for the Jack > Intermediate code translation. @DimitarYordanov17
-
-class JackTranslatorLibrary:
-    """
-    Main class to map the Jack language to intermediate (VM) code
-    """
-
-    return 0
