56e8db22c2e4c274ec126b95f61a27de374e7073
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-04-13 17:23:25 +0300

    refactor: refactor entire library

--- the-elements-of-computing-systems/jack-compiler/first-stage/jackTranslatorLibrary.py
+++ the-elements-of-computing-systems/jack-compiler/first-stage/jackTranslatorLibrary.py
@@ -1,13 +1,205 @@
 # An intermediate code library for the Jack > Intermediate code translation. @DimitarYordanov17
 # (This is a first-stage library - only Jack -> XML proccessing, tokenizng, cleaning and tabularizing)
+# (More info about the parsing logic at line 197)
 
 import re
-import copy
+
+class JackTranslatorLibrary:
+    """
+    A main library class capable of Jack language syntax analysis
+    """
+
+
+    SYNTAX_ELEMENTS = {
+
+        "symbols": ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~'],
+
+        "subroutines": ['constructor', 'function', 'method'],
+
+        "statements": ['let', 'if', 'while', 'do', 'return'],
+
+        "op": ['+', '-', '*', '/', '&', '|', '<', '>', '='],
+
+        "keywords": ['class', 'constructor', 'function',
+                     'method', 'field', 'static', 'var',
+                     'int', 'char', 'boolean', 'void', 'true',
+                     'false', 'null', 'this', 'let', 'do',
+                     'if', 'else', 'while', 'return'],
+    }
+
+
+    def parse_file(input_file_name):
+        """
+        Makes use of the JackTranslatorLibraryParser class to construct a .xml file from a .jack one
+        """
+
+        jack_parser = JackTranslatorLibraryParser(input_file_name)
+        jack_parser.parse()
+
+
+    def tokenize(input_file_name):
+        """
+        Tokenizes a file, spreading each keyword (token) on a newline
+        """
+
+        with open(input_file_name, 'r+') as input_file:
+            input_file.seek(0)
+            file_text = input_file.read()
+
+            # Clean trailing spaces
+            file_text = file_text.strip()
+
+            # Clean all \n
+            file_text = file_text.replace("\n", " ")
+
+            # Wider all symbols
+            for symbol in JackTranslatorLibrary.SYNTAX_ELEMENTS["symbols"]:
+                widened_symbol = " " + symbol + " "
+                file_text = file_text.replace(symbol, widened_symbol)
+
+            # Clean all in-file spaces
+            while "  " in file_text:
+                file_text = file_text.replace("  ", " ")
+
+            # Split into tokens
+            file_text = file_text.split()
+
+            input_file.seek(0)
+
+            for token in file_text:
+                classified_token = JackTranslatorLibrary._classify_token(token)
+                input_file.write(classified_token + '\n')
+
+            input_file.truncate()
+
+
+    def clean(input_file_name):
+        """
+        Removes all comments in a file, but keeps line spacing
+        (have to be careful to not remove a line which contains
+        division, signed by '/' or string, containing it)
+        """
+
+        with open(input_file_name, 'r+') as input_file:
+            lines = input_file.readlines()
+            input_file.seek(0)
+
+            in_comment = False
+
+            for line in lines:
+                if "/*" in line or "//" in line:
+                    comment = r"//" if "//" in line else r"/\*"
+                    string_to_write, enter_comment = JackTranslatorLibrary._proccess_comment(line, comment)
+
+                    in_comment = enter_comment
+
+                    if string_to_write != '\n' and string_to_write != '':
+                        input_file.write(string_to_write)
+
+                elif (len(line.strip()) >= 2) and ("*/" == (line.strip()[-2] + line.strip()[-1])):
+                    in_comment = False
+
+                elif (not in_comment) and line != '\n':
+                    input_file.write(line)
+
+            input_file.truncate()
+
+
+    def tabularize(input_file_name):
+        """
+        Indent every tag between two upper tags, keep nested depth
+        """
+
+        with open(input_file_name, 'r+') as input_file:
+            lines = input_file.readlines()
+            input_file.seek(0)
+
+            depth = 0
+
+            for line in lines:
+                tabularized_line = ('\t' * depth) + line
+
+                if " " not in line:
+                    if "/" in line:
+                        depth -= 1
+                        tabularized_line = ('\t' * depth) + line
+                    else:
+                        depth += 1
+
+                input_file.write(tabularized_line)
+
+
+    def _classify_token(token):
+        """
+        Appends a certain type tags to a token 
+        """
+
+        token_type = ""
+
+        if token in JackTranslatorLibrary.SYNTAX_ELEMENTS["keywords"]:
+            token_type = "keyword"
+
+        elif token in JackTranslatorLibrary.SYNTAX_ELEMENTS["symbols"]:
+            token_type = "symbol"
+
+        elif '"' in token_type:
+            token_type = "StringConstant"
+
+        elif token[0].isnumeric():
+            token_type = "integerConstant"
+
+        else:
+            token_type = "identifier"
+
+        classified_token = f"<{token_type}> {token} </{token_type}>"
+
+        return classified_token
+
+
+    def _proccess_comment(line, comment):
+        """
+        Returns a string, that should be written and a boolan to indicate if we are entering a block comment
+        """
+
+        line = line.rstrip()
+        comment_occurrences = [occurence.span()[0] for occurence in re.finditer(comment, line)]
+
+        string_to_write = line + '\n'
+        enter_block_comment = False
+
+        for comment_start_index in comment_occurrences:
+            before_comment_segment = line[:comment_start_index]
+            double_quotes_count = before_comment_segment.count('"')
+
+            if double_quotes_count % 2 == 0:  # We have a comment, because all possible string are closed
+                if before_comment_segment.replace(" ", ""):
+                    string_to_write = before_comment_segment + '\n'
+
+                else:
+                    string_to_write = ""
+
+                if "/*" in line:
+                    last_two_chars = line[-2] + line[-1]
+                    enter_block_comment = True
+
+                    if last_two_chars == "*/":
+                        enter_block_comment = False
+
+                break
+
+        return string_to_write, enter_block_comment
+
 
 class JackTranslatorLibraryParser:
     """
-    Additional class to hold the functions, needed for parsing a .jack file into .xml
-    (A cleaned and tokenized file should be passed)
+    Parse a cleaned, tokenized and classified file (Jack) and return the corresponding XML code
+
+    More information on the parsing logic:
+    // We start with the initializing of the class - make an instance, containing tokens (classified Jack keywords) and a row_pointer (used to
+    // indicate the current working index).Then we are parsing a file - call a parsing function (in this case _parse_class()) and then write the
+    // modified tokens to the file. The work of the _parse_class() function on the other side is pretty interesting - we start calling another
+    // inner functions, which call another inner functions. The whole proccess of parsing a file is sequential
+    // with recursive elements (e.g. expression parsing).
     """
 
     def __init__(self, input_file_name):
@@ -19,7 +211,7 @@ class JackTranslatorLibraryParser:
 
     def parse(self):
         """
-        Parse a file
+        Parse a single file
         """
 
         JackTranslatorLibraryParser._parse_class(self)
@@ -36,7 +228,7 @@ class JackTranslatorLibraryParser:
 
     def _parse_class(self):
         """
-        Parse a class
+        Parse a class (actually file parsing, but we know that there is only a single class in a file)
         """
 
         tag = "<class>\n"
@@ -49,9 +241,10 @@ class JackTranslatorLibraryParser:
         JackTranslatorLibraryParser._parse_variableDeclarations(self, class_vars=True)
         JackTranslatorLibraryParser._parse_subroutineDeclarations(self)
 
+
     def _parse_variableDeclarations(self, class_vars=False):
         """
-        Parse variable declarations. Node of _parse_class
+        Node of _parse_class
         """
 
         tag = "<classVarDec>\n" if class_vars else "<varDec>\n"
@@ -79,9 +272,10 @@ class JackTranslatorLibraryParser:
                     self.row_pointer -= 1
                     break
 
+
     def _parse_subroutineDeclarations(self):
         """
-        Parse subroutine declarations for a class. Node of _parse_class
+        Node of _parse_class
         """
 
         current_token = self.tokens[self.row_pointer]
@@ -90,9 +284,10 @@ class JackTranslatorLibraryParser:
             JackTranslatorLibraryParser._parse_subroutineDeclaration(self)
             current_token = self.tokens[self.row_pointer]
 
+
     def _parse_subroutineDeclaration(self):
         """
-        Parse a single subroutine declaration. Node of _parse_subroutineDeclarations
+        Node of _parse_subroutineDeclarations
         """
 
         dec_tag = "<subroutineDec>\n"
@@ -126,9 +321,10 @@ class JackTranslatorLibraryParser:
         self.tokens.insert(self.row_pointer, JackTranslatorLibraryParser._get_closed_tag(self, dec_tag))
         self.row_pointer += 1
 
+
     def _parse_subroutineBody(self):
         """
-        Parse all tokens in a subroutine body. Node of _parse_subroutineDeclaration
+        Node of _parse_subroutineDeclaration
         """
 
         tag = "<subroutineBody>\n"
@@ -144,9 +340,10 @@ class JackTranslatorLibraryParser:
         self.tokens.insert(self.row_pointer, JackTranslatorLibraryParser._get_closed_tag(self, tag))
         self.row_pointer += 1
 
+
     def _parse_statements(self, stop_value = "</subroutineDec>\n"):
         """
-        Parse subroutine body statements. Node of _parse_subroutineBody
+        Node of _parse_subroutineBody
         """
 
         tag = "<statements>\n"
@@ -195,7 +392,7 @@ class JackTranslatorLibraryParser:
 
     def _parse_let(self):
         """
-        Parse let statement. Node of _parse_statements
+        Node of _parse_statements
         """
 
         self.row_pointer += 2
@@ -215,9 +412,10 @@ class JackTranslatorLibraryParser:
             JackTranslatorLibraryParser._parse_expression(self)
             self.row_pointer += 1
 
+
     def _parse_do(self):
         """
-        Parse do statement. Node of _parse_statements
+        Node of _parse_statements
         """
 
         self.row_pointer += 1
@@ -233,9 +431,10 @@ class JackTranslatorLibraryParser:
 
         self.row_pointer += 2
 
+
     def _parse_if(self):
         """
-        Parse if statement. Node of _parse_statements
+        Node of _parse_statements
         """
 
         self.row_pointer += 2
@@ -257,9 +456,10 @@ class JackTranslatorLibraryParser:
 
             self.row_pointer += 1
 
+
     def _parse_while(self):
         """
-        Parse while statement. Node of _parse_statements
+        Node of _parse_statements
         """
 
         self.row_pointer += 2
@@ -272,9 +472,10 @@ class JackTranslatorLibraryParser:
 
         self.row_pointer += 1
 
+
     def _parse_return(self):
         """
-        Parse return statement. Node of _parse_statements
+        Node of _parse_statements
         """
 
         self.row_pointer += 1
@@ -288,11 +489,11 @@ class JackTranslatorLibraryParser:
         JackTranslatorLibraryParser._parse_expression(self)
         self.row_pointer += 1
         
-
     # ~~~~~~~~~~~~ Statement auxiliary parsing ~~~~~~~~~~~~~~~~~
+
     def _parse_expression(self):
         """
-        Parse an expression. Node of _parse_expressionList
+        Node of _parse_expressionList
         """
 
         tag = "<expression>\n"
@@ -311,9 +512,10 @@ class JackTranslatorLibraryParser:
         self.tokens.insert(self.row_pointer, JackTranslatorLibraryParser._get_closed_tag(self, tag))
         self.row_pointer += 1
 
+
     def _parse_expression_list(self):
         """
-        Parse expression list. Node of _parse_let;do;if;while;return
+        Node of _parse_let;do;if;while;return
         """
         current_token = JackTranslatorLibraryParser._get_token_value(self, self.tokens[self.row_pointer])
 
@@ -337,9 +539,10 @@ class JackTranslatorLibraryParser:
         self.tokens.insert(self.row_pointer, JackTranslatorLibraryParser._get_closed_tag(self, tag))
         self.row_pointer += 1
 
+
     def _parse_term(self):
         """
-        Parse term - recursion might be used. Node of _parse_expression
+        Node of _parse_expression
         """
 
         tag = "<term>\n"
@@ -384,6 +587,7 @@ class JackTranslatorLibraryParser:
         self.row_pointer += 1
 
     # ~~~~~~~~~~~ Tag auxiliary ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     def _get_closed_tag(self, tag):
         """
         Appends '/' in the second place of the input
@@ -393,6 +597,7 @@ class JackTranslatorLibraryParser:
         tag_list.insert(1, "/")
         return "".join(tag_list)
 
+
     def _get_token_value(self, tag):
         """
         Returns mediocre keyword
@@ -401,181 +606,3 @@ class JackTranslatorLibraryParser:
             return tag.split()[1]
         except:
             return tag
-
-
-
-class JackTranslatorLibrary:
-    """
-    Main class to map the Jack language to intermediate (VM) code
-    """
-    SYNTAX_ELEMENTS = {
-
-        "symbols": ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~'],
-
-        "keywords": ['class', 'constructor', 'function',
-                     'method', 'field', 'static', 'var',
-                     'int', 'char', 'boolean', 'void', 'true',
-                     'false', 'null', 'this', 'let', 'do',
-                     'if', 'else', 'while', 'return'],
-
-        "subroutines": ['constructor', 'function', 'method'],
-
-        "statements": ['let', 'if', 'while', 'do', 'return'],
-
-        "op": ['+', '-', '*', '/', '&', '|', '<', '>', '='],
-
-    }
-
-    def parse_file(input_file_name):
-        """
-        Constructs a parser and returns parsed XML code
-        """
-        jack_parser = JackTranslatorLibraryParser(input_file_name)
-        jack_parser.parse()
-
-    def tokenize(input_file_name):
-        """
-        Tokenizes a file, spreading each keyword on a newline
-        """
-
-        with open(input_file_name, 'r+') as input_file:
-            input_file.seek(0)
-            file_text = input_file.read()
-
-            # Clean trailing spaces
-            file_text = file_text.strip()
-
-            # Clean all \n
-            file_text = file_text.replace("\n", " ")
-
-            # Wider all symbols
-            for symbol in JackTranslatorLibrary.SYNTAX_ELEMENTS["symbols"]:
-                widened_symbol = " " + symbol + " "
-                file_text = file_text.replace(symbol, widened_symbol)
-
-            # Clean all in-file spaces
-            while "  " in file_text:
-                file_text = file_text.replace("  ", " ")
-
-            # Split into tokens
-            file_text = file_text.split()
-
-            input_file.seek(0)
-
-            for token in file_text:
-                classified_token = JackTranslatorLibrary._classify_token(token)
-                input_file.write(classified_token + '\n')
-
-            input_file.truncate()
-
-    def _classify_token(token):
-        """
-        Append tags to a token
-        """
-        token_type = ""
-
-        if token in JackTranslatorLibrary.SYNTAX_ELEMENTS["keywords"]:
-            token_type = "keyword"
-
-        elif token in JackTranslatorLibrary.SYNTAX_ELEMENTS["symbols"]:
-            token_type = "symbol"
-
-        elif '"' in token_type:
-            token_type = "StringConstant"
-
-        elif token[0].isnumeric():
-            token_type = "integerConstant"
-
-        else:
-            token_type = "identifier"
-
-        classified_token = f"<{token_type}> {token} </{token_type}>"
-
-        return classified_token
-
-    def _proccess_comment(line, comment):
-        """
-        Returns a string, which is parsed from the line and a boolean, which signals if we are entering a block comment
-        """
-
-        line = line.rstrip()
-        comment_occurrences = [occurence.span()[0] for occurence in re.finditer(comment, line)]
-
-        string_to_write = line + '\n'
-        enter_block_comment = False
-
-        for comment_start_index in comment_occurrences:
-            before_comment_segment = line[:comment_start_index]
-            double_quotes_count = before_comment_segment.count('"')
-
-            if double_quotes_count % 2 == 0:  # We have a comment, because all possible string are closed
-                if before_comment_segment.replace(" ", ""):
-                    string_to_write = before_comment_segment + '\n'
-
-                else:
-                    string_to_write = ""
-
-                if "/*" in line:
-                    last_two_chars = line[-2] + line[-1]
-                    enter_block_comment = True
-
-                    if last_two_chars == "*/":
-                        enter_block_comment = False
-
-                break
-
-        return string_to_write, enter_block_comment
-
-    def clean(input_file_name):
-        """
-        Removes all comments in a file, but keeps line spacing
-        (have to be careful to not remove a line which contains
-        division, signed by '/' or string, containing it)
-        """
-
-        with open(input_file_name, 'r+') as input_file:
-            lines = input_file.readlines()
-            input_file.seek(0)
-
-            in_comment = False
-
-            for line in lines:
-                if "/*" in line or "//" in line:
-                    comment = r"//" if "//" in line else r"/\*"
-                    string_to_write, enter_comment = JackTranslatorLibrary._proccess_comment(line, comment)
-
-                    in_comment = enter_comment
-
-                    if string_to_write != '\n' and string_to_write != '':
-                        input_file.write(string_to_write)
-
-                elif (len(line.strip()) >= 2) and ("*/" == (line.strip()[-2] + line.strip()[-1])):
-                    in_comment = False
-
-                elif (not in_comment) and line != '\n':
-                    input_file.write(line)
-
-            input_file.truncate()
-
-    def tabularize(input_file_name):
-        """
-        Indent every tag between two upper tags, keep nested depth
-        """
-
-        with open(input_file_name, 'r+') as input_file:
-            lines = input_file.readlines()
-            input_file.seek(0)
-
-            depth = 0
-
-            for line in lines:
-                tabularized_line = ('\t' * depth) + line
-
-                if " " not in line:
-                    if "/" in line:
-                        depth -= 1
-                        tabularized_line = ('\t' * depth) + line
-                    else:
-                        depth += 1
-
-                input_file.write(tabularized_line)
