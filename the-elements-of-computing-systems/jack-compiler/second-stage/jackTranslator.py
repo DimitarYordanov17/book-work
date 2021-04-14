dc256c9e7120b7c0e6c76f420645c19798d3d08b
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-04-14 15:42:17 +0300

    final translator blueprint

--- the-elements-of-computing-systems/jack-compiler/second-stage/jackTranslator.py
+++ the-elements-of-computing-systems/jack-compiler/second-stage/jackTranslator.py
@@ -1,6 +1,6 @@
 # A jack translator (front-end). Jack code to Intermediate code. @DimitarYordanov17
 
-# To run: python3 jackTranslator.py {path}
+# To run: python3 jackTranslator.py {path} {construct corresponding XML files, yes/no}
 
 from jackTranslatorLibrary import JackTranslatorLibrary
 import os
@@ -13,28 +13,36 @@ class JackTranslator:
     """
 
 
-    def translate(path):
+    def translate(path, construct_xml=False):
         """
         Translate a directory/file, .jack -> .vm
         """
-        
+       
         jack_files = []
+        
+        if ".jack" in path: # Single file
+            jack_files.append(path)
 
-        for root, dirs, files in os.walk(path):
-            for file_name in files:
-                if ".jack" in file_name:
-                  jack_files.append(file_name)
+        else:
+            for root, dirs, files in os.walk(path):
+                for file_name in files:
+                    if ".jack" in file_name:
+                      jack_files.append(file_name)
         
         for jack_file_name in jack_files:
             output_file_name = jack_file_name.split(".")[0] + ".vm"
             
             vm_code = JackTranslatorLibrary.translate_file(jack_file_name)
-
+            
             with open(output_file_name, 'w') as output_file:
                 for line in vm_code:
                     output_file.write(line)
 
+                output_file.truncate()
 
+            if construct_xml:
+                JackTranslator._construct_xml(jack_file_name)
+        
     def _construct_xml(input_file_name):
         """
         Parses a single .jack file, resulting in a .xml file
@@ -51,4 +59,4 @@ class JackTranslator:
         JackTranslatorLibrary.tabularize(output_file_name)
 
 
-JackTranslator.translate(sys.argv[1])
+JackTranslator.translate(sys.argv[1], construct_xml = True if sys.argv[2] == "yes" else False)
