329a74da3354643cc413c2aa4c9eba4a44b8251c
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-04-26 16:48:13 +0300

    implement standard library functionalities

--- the-elements-of-computing-systems/jack-compiler/second-stage/jackStandardLibrary.py
+++ the-elements-of-computing-systems/jack-compiler/second-stage/jackStandardLibrary.py
@@ -10,15 +10,38 @@ class JackStandardLibrary:
     Raw text from the book
 
     Standard library formatted:
-    class_name: {function_name1: [function_kind, function_type, function_parameter_list], function_name2: [function_kind, function_type, function_parameter_list]}
+    class_name: {subroutine_name1: [subroutine_kind, subroutine_type, subroutine_parameter_list], subroutine_name2: [subroutine_kind, subroutine_type, subroutine_parameter_list]}
     """
     
-    standard_library_raw = open('jackStandardLibraryRaw.txt', 'r').read()
-    standard_library_formatted = JackStandardLibrary.construct_formatted_library()
-
-    def construct_formatted_library():
+    def __init__(self, file_name='jackStandardLibraryRaw.txt'):
+        standard_library_raw = open(file_name, 'r').read()
+        standard_library_formatted = JackStandardLibrary.construct_formatted_library(standard_library_raw)
+       
+    def construct_formatted_library(text):
         """
         Format the raw library text into segment dictionaries
         """
+        
+        dictionary = {}
+        
+        classes_declarations = re.split(r'[a-zA-Z]+\n\n', text)
+        classes_names = [class_name.split('\n')[0] for class_name in re.findall(r'[a-zA-Z]+\n\n', text)]
 
-        return 0
+        for class_name, class_dec in zip(classes_names, classes_declarations[1:]):
+            methods_and_functions_declarations = re.findall(r'((constructor|function|method).+:)', class_dec)
+            cleaned_methods_functions = [pair[0][:-1] for pair in methods_and_functions_declarations]
+            
+            classified_methods_functions = {}
+            
+            for subroutine_dec in cleaned_methods_functions:
+                subroutine_parts = subroutine_dec.split()
+                subroutine_name = subroutine_parts[2]
+                subroutine_kind = subroutine_parts[0]
+                subroutine_type = subroutine_parts[1]
+                parameter_list = subroutine_dec[subroutine_dec.index('('):]
+
+                classified_methods_functions[subroutine_name] = [subroutine_kind, subroutine_type, parameter_list]
+
+            dictionary[class_name] = classified_methods_functions
+
+        return dictionary
